209b48b178eca334c183ae48c95931ce69cf67fd
SomeNerdWithAPC <87348180+SomeNerdWithAPC@users.noreply.github.com>
2021-11-02 16:48:29 -0400

    Update dnd.js

--- dnd.js
+++ dnd.js
@@ -1,20 +1,15 @@
 const Discord = require('discord.js')
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES']});
 const PREFIX = "!"
-NotOn = "@here"
-PCC = 0
-PCV = 0
+
 
 client.on('ready', () =>{
    console.log('Time for DND');
-   client.user.setActivity('Dungeoning those Dragons')
+   client.user.setActivity('Dungeoning those dragons', { type: 'PLAYING' })
 })
 
-client.on("message", message => {
-            
-    
-        
-        
+client.on("messageCreate", message => {
+
     let args = message.content.substring(PREFIX.length).split(" ");
    
        switch(args[0]){
@@ -23,7 +18,7 @@ client.on("message", message => {
            ATTRD = message.content.charAt(0)
            for (let  Lettuce = 0; Lettuce < ATTRD; Lettuce++) {
            rolld8 = Math.floor((Math.random() *8) + 1);
-           message.channel.send(rolld8)
+           message.reply(rolld8.toString())
            }
            break;
            case 'd20': 
@@ -34,11 +29,10 @@ client.on("message", message => {
            rolld20IG = Math.floor((Math.random() *20) + 1);
            rolld20 = rolld20IG + 1
            if (rolld20 > 19) {
-            message.channel.send(rolld20)
-            message.channel.send ("Nat 20! Nice!");
+            message.reply("Nat 20! Nice!");
            }
            else {
-                message.channel.send(rolld20)
+                message.reply(rolld20.toString())
          }
          }
             break;
@@ -47,7 +41,7 @@ client.on("message", message => {
                ATTRD = message.content.charAt(0)
            for (let  Lettuce = 0; Lettuce < ATTRD; Lettuce++) { 
            rolld12 = Math.floor((Math.random() *12) + 1);
-           message.channel.send (rolld12)
+           message.reply(rolld12.toString())
            }
            break;
            
@@ -55,21 +49,21 @@ client.on("message", message => {
            ATTRD = message.content.charAt(0)
            for (let  Lettuce = 0; Lettuce < ATTRD; Lettuce++) {
            rolld6 = Math.floor((Math.random() *6) + 1);
-           message.channel.send (rolld6)
+           message.reply(rolld6.toString())
            }
             break;
             case 'd4': 
             ATTRD = message.content.charAt(0)
            for (let  Lettuce = 0; Lettuce < ATTRD; Lettuce++) {
            rolld4 = Math.floor((Math.random() *4) + 1);
-           message.channel.send (rolld4)
+           message.reply(rolld4.toString())
            }
             break;
             case 'd10': 
             ATTRD = message.content.charAt(0)
            for (let  Lettuce = 0; Lettuce < ATTRD; Lettuce++) {
             rolld10 = Math.floor((Math.random() *10) + 1);
-            message.channel.send (rolld10)
+            message.reply(rolld10.toString())
            }
             break;
 //this command has been removed, If you'd like to complain about this decision, DM me on discord @cash#2258
@@ -84,16 +78,16 @@ client.on("message", message => {
                   RWTR = WTR.substring(1)
                      if (RWTR < 100) {
                        PRolled = Math.floor((Math.random() *RWTR) + 1);
-                        message.channel.send(PRolled)
+                        message.channel.send(PRolled.toString())
                            if (TTR > 99); {
                            }  for (let  Lettuce = 1; Lettuce < TTR; Lettuce++) {
                                  RWTR = WTR.substring(1)
-                                 Rolled = Math.floor((Math.random() *RWTR) + 1);
-                              }   message.channel.send(Rolled)
+                                 CRolled = Math.floor((Math.random() *RWTR) + 1);
+                              }   message.reply(CRolled.toString())
                      
-                    } else if (RWTR > 100) {message.channel.send ("Sorry, you an only roll up to a d100, this is to prevent spam and my computer from exploding")}        
+                    } else if (RWTR > 100) {message.reply("Sorry, you an only roll up to a d100, this is to prevent spam and my computer from exploding")}        
             
-               } else if (TTR > 14) {message.channel.send ("Sorry, you an only roll up to 15 dice at a time, this is to prevent spam and my computer from exploding")}
+               } else if (TTR > 15) {message.reply("Sorry, you an only roll up to 15 dice at a time, this is to prevent spam and my computer from exploding")}
          
             }
             break;         
@@ -102,4 +96,4 @@ client.on("message", message => {
 }})
 
 
-client.login('REMOVED');
+client.login(No);
